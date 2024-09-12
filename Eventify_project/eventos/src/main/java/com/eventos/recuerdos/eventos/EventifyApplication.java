66ce9d017af027d60916295400e37f884ed43c5b
salaverryyy <david.salaverry@utec.edu.pe>
2024-09-12 14:13:43 -0500

    Inicio de proyecto

---
+++ Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventos/EventifyApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.recuerdos.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyApplication.class, args);
+	}
+
+}
