cea284ebc31376a7bdfc9da79d32324615253944
salaverryyy <david.salaverry@utec.edu.pe>
2024-09-12 14:13:43 -0500

    Inicio de proyecto

---
+++ Eventify_project/eventos/src/test/java/com/eventos/recuerdos/eventos/EventifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.recuerdos.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
