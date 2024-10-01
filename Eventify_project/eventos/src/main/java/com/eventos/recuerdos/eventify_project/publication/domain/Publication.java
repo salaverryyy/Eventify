cd8646a7ef28924e7193559edaa2f097bb18d164
salaverryyy <david.salaverry@utec.edu.pe>
2024-09-30 23:59:52 -0500

    Entidad Publication lista

--- Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/publication/domain/Publication.java
+++ Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/publication/domain/Publication.java
@@ -7,12 +7,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class Publication {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private User user;
-
+    private Long id; //id unico
+    private User user; //usuario que hace la publicacion
+    private TipoArchivo tipoArchivo;// tipo de archivo(FOTO o VIDEO)
+    private String archivo; //URL del archivo
+    private String descripcion; //descripcion de la publicacion
+    private LocalDate fechaPublicacion; //fecha de publicacion
+    private int likes; //cantidad de Likes
+    private List<Comentario> comentarios; // lista de comentarios de la publicacion
 
 }
