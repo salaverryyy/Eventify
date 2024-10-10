dcf6b0b9f74e91da5ee44f7e9c425771d6e4abe7
salaverryyy <david.salaverry@utec.edu.pe>
2024-10-10 11:34:53 -0500

    Cambio con el nombre del Endponit de Like a likePublication

--- Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/like/application/LikeController.java
+++ Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/like/application/LikeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/like")
+@RequestMapping("/likePublication")
 public class LikeController {
     @Autowired
     private LikeService likeService;
