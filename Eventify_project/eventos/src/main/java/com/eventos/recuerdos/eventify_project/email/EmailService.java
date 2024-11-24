6d2bbe3cc4d0dd9a9c9892ce6a4ea876505b487e
Giordano Fuentes <giordimega@hotmail.com>
2024-11-24 14:08:34 -0500

    foto de email arreglada

--- Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/email/EmailService.java
+++ Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/email/EmailService.java
@@ -1,6 +1,8 @@
 package com.eventos.recuerdos.eventify_project.email;
+import com.eventos.recuerdos.eventify_project.controllers.StorageService;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import java.io.File;
 @Service
 public class EmailService {
 
+    @Autowired
+    private StorageService storageService;
     @Autowired
     private JavaMailSender mailSender;
 
@@ -34,10 +38,20 @@ public class EmailService {
             helper.setSubject(subject);
             helper.setText(contentHTML, true); // true para indicar que es HTML
 
-            // Adjunta la imagen como recurso interno
-            File imageFile = new File("C:/DAVID/CS/Ciclo4/DBP/Proyecto/LOGO/Eventify_LOGO.jpg");
-            helper.addInline("logoImage", imageFile);
+            try {
+                // Generate the presigned URL
+                String presignedUrl = storageService.generatePresignedUrl("eventiyLogo.jpg");
 
+                // Fetch the resource from the URL
+                UrlResource resource = new UrlResource(presignedUrl);
+
+                // Attach the resource to the email as an inline image
+                helper.addInline("logoImage", resource, "image/jpeg");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error attaching logo to email", e);
+            }
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             e.printStackTrace();
@@ -60,10 +74,20 @@ public class EmailService {
             helper.setSubject(subject);
             helper.setText(contentHTML, true);
 
-            // Adjuntar el logo (opcional)
-            File imageFile = new File("C:/DAVID/CS/Ciclo4/DBP/Proyecto/LOGO/Eventify_LOGO.jpg");
-            helper.addInline("logoImage", imageFile);
+            try {
+                // Generate the presigned URL
+                String presignedUrl = storageService.generatePresignedUrl("eventiyLogo.jpg");
 
+                // Fetch the resource from the URL
+                UrlResource resource = new UrlResource(presignedUrl);
+
+                // Attach the resource to the email as an inline image
+                helper.addInline("logoImage", resource, "image/jpeg");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error attaching logo to email", e);
+            }
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             e.printStackTrace();
