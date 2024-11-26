82fc9cff5c04d17b0943566a51fc141bd9f5779d
salaverryyy <david.salaverry@utec.edu.pe>
2024-11-26 13:23:45 -0500

    BACKEND FUNCIONAL - COMPLETO - Pequeño cambio en Event

--- Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/config/SecurityConfig.java
+++ Eventify_project/eventos/src/main/java/com/eventos/recuerdos/eventify_project/config/SecurityConfig.java
@@ -84,4 +84,7 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+
+
 }
